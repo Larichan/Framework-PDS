d24472972567f0ffdf58dd98bc597a82d37b01df
Bledson Kivy <bledson@gmail.com>
2015-05-31 23:45:36 -0300

    init dev on quiz game

--- src/core/Screen.java
+++ src/core/Screen.java
@@ -1,4 +1,7 @@
 package core;
+
+import java.io.IOException;
+
 public interface Screen extends Colleague {
-	public void draw();
+	public void draw() throws IOException;
 }
--- src/core/Start.java
+++ src/core/Start.java
@@ -1,3 +1,13 @@
 package core;
+
+import quiz.ConcreteStartScreen;
+
+import java.io.IOException;
+
 public class Start {
+
+    public static void main(String[] args) throws IOException {
+        StartScreen start_screen = new ConcreteStartScreen();
+        start_screen.draw();
+    }
 }
